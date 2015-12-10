8eff88c184dbdff2826e13e12801bc0fe1468e5f
Lyon <lozybean@163.com>
2015-12-10 23:14:46 +0800

    clone to mac

--- learn/views.py
+++ learn/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
 from .models import Person
+
 
 # Create your views here.
 
 def home(request):
-    print list(request.META.iterkeys())
+    # print list(request.META.iterkeys())
     vars = {
         'user': request.user,
         'address': request.get_host(),
